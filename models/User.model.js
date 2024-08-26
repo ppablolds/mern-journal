7970fa1bba58231d644109d03b3c69060ca8f10f
pablo silva <pablosilvaaluno@outlook.com>
2024-08-26 15:08:34 -0300

    install and add bcrypt

--- models/User.model.js
+++ models/User.model.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, require: true },
   username: { type: String, require: true },
   email: { type: String, require: true, unique: true, lowercase: true },
   password: { type: String, require: true, select: false },
+});
+
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
