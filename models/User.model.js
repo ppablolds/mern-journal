f590c9ba794e1bb03ead6c8e65c6b7155e6c55dc
pablo silva <pablosilvaaluno@outlook.com>
2024-08-26 14:40:58 -0300

    corrigindo erro

--- models/User.model.js
+++ models/User.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, require: true },
   username: { type: String, require: true },
   email: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
+  password: { type: String, require: true, select: false },
 });
 
 const User = mongoose.model("User", userSchema);
