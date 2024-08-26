0b684dcba10dd9711c77a4c57c514de2aa40dbc4
pablo silva <pablosilvaaluno@outlook.com>
2024-08-26 14:42:32 -0300

    add lowercase

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: { type: String, require: true },
   username: { type: String, require: true },
-  email: { type: String, require: true, unique: true },
+  email: { type: String, require: true, unique: true, lowercase: true },
   password: { type: String, require: true, select: false },
 });
 
