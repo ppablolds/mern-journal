bc49b23586be2b679521a86d539d557c0c6d4080
pablo silva <pablosilvaaluno@outlook.com>
2024-08-27 09:39:18 -0300

    create News model

---
+++ src/models/News.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const newsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  banner: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: {
+    type: Array,
+    required: true,
+  },
+  comments: {
+    type: Array,
+    required: true,
+  },
+});
+
+const News = mongoose.model("News", newsSchema);
+
+export default News;
