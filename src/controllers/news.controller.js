220d0b507a4319d5096a1d6012209a9b0d913c9a
pablo silva <pablosilvaaluno@outlook.com>
2024-08-28 09:26:07 -0300

    make the pagination in getAllPostsController

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -23,12 +23,52 @@ const createPostController = async (req, res) => {
 
 const getAllPostController = async (req, res) => {
   try {
-    const posts = await newsService.getAllPostsService();
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+      limit = 5;
+    }
+
+    if (!offset) {
+      offset = 0;
+    }
+
+    const posts = await newsService.getAllPostsService(offset, limit);
+
+    const total = await newsService.countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previus = offset - limit < 0 ? null : offset - limit;
+    const previusUrl =
+      previus != null ? `${currentUrl}?limit=${limit}&offset=${previus}` : null;
 
     if (posts.length === 0) {
       res.status(400).json({ message: "Não há usuários." });
     }
-    res.status(200).json(posts);
+    res.status(200).json({
+      limit,
+      offset,
+      total,
+      nextUrl,
+      previusUrl,
+      results: posts.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+      })),
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -1,6 +1,8 @@
 import News from "../models/News.model.js";
 
 const createPostService = (body) => News.create(body);
-const getAllPostsService = () => News.find();
+const getAllPostsService = (offset, limit) =>
+  News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
+const countNews = () => News.countDocuments();
 
-export default { createPostService, getAllPostsService };
+export default { createPostService, getAllPostsService, countNews };
